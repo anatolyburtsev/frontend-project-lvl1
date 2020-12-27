884eb6c9ed1a7525f0ddb3598a6ba2db55db5de3
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2020-12-26 16:46:15 -0800

    add elint with airbnb rules

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import sayHello from "./cli.js";
+import sayHello from './cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 sayHello();
