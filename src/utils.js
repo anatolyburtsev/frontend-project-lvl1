5ec57db1f09915747fa5b80c3c440a006851122b
anatoliiburtsev <anatolyburtsev@yandex.ru>
2021-01-05 20:00:11 -0800

    add helpers back, which was renamed to utils

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+const generateRandomNumber = (minNumber, maxNumber) => {
+  const min = Math.ceil(minNumber);
+  const max = Math.floor(maxNumber) + 1;
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const pickRandom = (arr) => {
+  const index = generateRandomNumber(0, arr.length - 1);
+  const value = arr[index];
+  return { index, value };
+};
+
+export { generateRandomNumber, pickRandom };
