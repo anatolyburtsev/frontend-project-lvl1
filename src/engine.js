c5df5475b7298c38430b87e6a5050314ab8e00c7
anatoliiburtsev <anatolyburtsev@yandex.ru>
2021-01-11 22:35:48 -0800

    lint-fix

--- src/engine.js
+++ src/engine.js
@@ -13,7 +13,7 @@ const build = (description, generateRound, roundCount = 3) => {
 
     if (answer !== correctAnswer) {
       console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
       );
       console.log(`Let's try again, ${name}!`);
       return false;
